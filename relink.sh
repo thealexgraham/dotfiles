fa797c983257290c85c984b7008f13763f71cb10
Alex Graham <alex.graham@ampermusic.com>
2016-09-06 12:35:41 -0400

    Added linking file

---
+++ relink.sh
@@ -0,0 +1,10 @@
+#mv ~/.ghci ~/.ghci.old 
+#mv ~/.vimrc ~/.vimrc.old
+#rv ~/.zshenv ~/.zshenv.old
+#mv ~/.zshrc ~/.zshrc.old
+#mv ~/.tmux.conf ~/.tmux.conf.old
+ln -f ghci ~/.ghci
+ln -f vimrc ~/.vimrc
+ln -f zshenv ~/.zshenv
+ln -f zshrc ~/.zshrc
+ln -f tmux.conf ~/.tmux.conf
